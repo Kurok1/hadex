ef8cefa7153946e1cf0c837beb9550eac9e20f1c
Kurok1 <khanc.dev@gmail.com>
2025-12-21 17:55:13 +0800

    保持主题持久化 & 优化docker打包

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,9 +1,11 @@
+
 import { hasLocale, NextIntlClientProvider } from 'next-intl';
 import { notFound } from 'next/navigation';
 import { Inter, Geist, Geist_Mono } from "next/font/google";
 import { routing } from '@/i18n/routing'
 import { Locale } from '@/i18n/config'
 import { setRequestLocale } from 'next-intl/server';
+import { ThemeProvider } from 'next-themes';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -45,8 +47,14 @@ export default async function RootLayout({ children, params }: Props) {
   setRequestLocale(locale);
   
   return (
-    <NextIntlClientProvider>
-      {children}
-    </NextIntlClientProvider>
+    <NextIntlClientProvider locale={locale}>
+          <ThemeProvider 
+            attribute="data-theme" 
+            defaultTheme="light"
+            enableSystem={false} // 如果你只想在 light 和 dracula 切换，建议禁用系统同步
+          >
+            {children}
+          </ThemeProvider>
+        </NextIntlClientProvider>
   )
 }
